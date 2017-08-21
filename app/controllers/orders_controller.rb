17adbf06f9e937705ed714c1f920a3065024887c
Carissa <carissa@edgren.nu>
2017-08-21 10:51:37 +0200

    fixed PR comments

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,36 +1,25 @@
 class OrdersController < ApplicationController
-  before_action :get_order
-  before_action :get_referer
 
   def create
-    dish = Dish.find(params[:dish_id])
-    if @order.add(dish, dish.price)
-      flash[:notice] = "#{dish.title} was successfully added to order!"
+    session[:return_to] ||= request.referer
+
+    if session[:order_id]
+      order = Order.find(session[:order_id])
+      if order == 0
+        order = Order.create
+        session[:order_id] = order.id
+      end
     else
-      flash[:alert] = 'Item not added, try again!'
+      order = Order.create
+      session[:order_id] = order.id
+
     end
+
+   dish = Dish.find(params[:dish_id])
+     order.add(dish, dish.price)
+      flash[:notice] = dish.title + " was  successfully added to order!"
+
     redirect_to session.delete(:return_to)
   end
 
-<<<<<<< HEAD
-=======
-  def index
-    #@order_items = Order.find_by(id: session[:order_id]).all
-    #@order = Order.last
-  end
-
-  private
-  def get_order
-    if session[:order_id]
-      @order = Order.find(session[:order_id])
-    else
-      @order = Order.create
-      session[:order_id] = @order.id
-    end
-  end
-
-  def get_referer
-    session[:return_to] ||= request.referer
-  end
->>>>>>> 6b17e3a37c6babc0336605f6c03d50e41a8ac491
 end
