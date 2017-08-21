941d765f3f003d06b0a818071784da1437a16bf2
Carissa <carissa@edgren.nu>
2017-08-21 15:49:57 +0200

    Removed unnecessary migration file

--- db/migrate/20170818100414_change_restaurants_reference.rb
+++
@@ -1,6 +0,0 @@
- class ChangeRestaurantsReference < ActiveRecord::Migration[5.1]
-#   def change
-#     remove_reference :dishes, :restaurants, foreign_key: true
-#     add_reference :dishes, :restaurant, foreign_key: true
-#   end
- end
