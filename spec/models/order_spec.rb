12ba6a0ee90753474cfc69a85a3a7a2cada79839
tochman <thomas@craftacademy.se>
2017-08-21 05:59:39 +0200

    adds tax_pct spec to order

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,4 +1,8 @@
 RSpec.describe Order, type: :model do
   it { is_expected.to have_db_column :created_at }
   it { is_expected.to have_db_column :updated_at }
+
+  it 'has 0 tax' do
+    expect(subject.tax_pct).to eq 0
+  end
 end
