bc9dc6b70996a73b7a1ad8698120c21a002dd259
tochman <thomas@craftacademy.se>
2017-08-21 20:23:08 +0200

    adds content to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.create(name: 'Indian Fun', address1: 'Snorgatan 12', postal_code: '164 55', city: 'Kista', cuisine: 'Indian Food')
+Restaurant.create(name: 'Italian Fun', address1: 'Leyongatan 14', postal_code: '164 55', city: 'Kista', cuisine: 'Italian Food')
+Restaurant.create(name: 'Chinese Fun', address1: 'Potatisgatan 20', postal_code: '164 55', city: 'Kista', cuisine: 'Chinese Food')
+Restaurant.create(name: 'American Fun', address1: 'Snigelgatan 69', postal_code: '164 55', city: 'Kista', cuisine: 'American Food')
+
+Dish.create(title: 'papadom', description: 'deep fried crunchy things', price: 100, restaurant: Restaurant.first)
