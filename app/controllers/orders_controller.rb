cb91ce954591a0a37df925e0bd1096cae5b0aa16
j0sv <svensson.jonas@gmail.com>
2017-08-21 14:25:47 +0200

    Modify order controller and refactor index

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,25 +1,33 @@
 class OrdersController < ApplicationController
+  before_action :get_order
+  before_action :get_referer
 
   def create
-    session[:return_to] ||= request.referer
-
-    if session[:order_id]
-      order = Order.find(session[:order_id])
-      if order == 0
-        order = Order.create
-        session[:order_id] = order.id
-      end
+    dish = Dish.find(params[:dish_id])
+    if @order.add(dish, dish.price)
+      flash[:notice] = "#{dish.title} was successfully added to order!"
     else
-      order = Order.create
-      session[:order_id] = order.id
-
+      flash[:alert] = 'Item not added, try again!'
     end
-
-   dish = Dish.find(params[:dish_id])
-     order.add(dish, dish.price)
-      flash[:notice] = dish.title + " was  successfully added to order!"
-
     redirect_to session.delete(:return_to)
   end
 
+  def index
+    @order = Order.find_by(id: session[:order_id])
+    @order_items = @order.shopping_cart_items
+  end
+
+  private
+  def get_order
+    if session[:order_id]
+      @order = Order.find(session[:order_id])
+    else
+      @order = Order.create
+      session[:order_id] = @order.id
+    end
+  end
+
+  def get_referer
+    session[:return_to] ||= request.referer
+  end
 end
--- db/migrate/20170818100414_change_restaurants_reference.rb
+++ db/migrate/20170818100414_change_restaurants_reference.rb
@@ -1,6 +1,6 @@
-class ChangeRestaurantsReference < ActiveRecord::Migration[5.1]
-  def change
-    remove_reference :dishes, :restaurants, foreign_key: true
-    add_reference :dishes, :restaurant, foreign_key: true
-  end
-end
+ class ChangeRestaurantsReference < ActiveRecord::Migration[5.1]
+#   def change
+#     remove_reference :dishes, :restaurants, foreign_key: true
+#     add_reference :dishes, :restaurant, foreign_key: true
+#   end
+ end
