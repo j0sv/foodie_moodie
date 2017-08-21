5ffc636d352f3b80d7f238362f9fc111fcad2e5a
faraznaeem <faraznaeem@gmail.com>
2017-08-21 16:44:45 +0200

    create location_steps.rb

---
+++ features/step_definitions/location_steps.rb
@@ -0,0 +1,7 @@
+Then(/^I should see map$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^A pointer for my location$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
