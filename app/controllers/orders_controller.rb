87d3cee99e8b250dfbc7873b4016bc5324bd2b61
tochman <thomas@craftacademy.se>
2017-08-21 05:51:15 +0200

    rafactors orders controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,26 +1,33 @@
 class OrdersController < ApplicationController
+  before_action :get_order
+  before_action :get_referer
 
   def create
-    session[:return_to] ||= request.referer
-
-    if session[:order_id]
-      order = Order.find(session[:order_id])
+    dish = Dish.find(params[:dish_id])
+    if @order.add(dish, dish.price)
+      flash[:notice] = "#{dish.title} was successfully added to order!"
     else
-      order = Order.create
-      session[:order_id] = order.id
-    end
-
-   dish = Dish.find(params[:dish_id])
-    if order.add(dish, dish.price)
-      flash[:notice] = dish.title + " was  successfully added to order!"
-    else
-      flash[:alert] = "Item not added, try again!"
+      flash[:alert] = 'Item not added, try again!'
     end
     redirect_to session.delete(:return_to)
   end
 
- def index
+  def index
     #@order_items = Order.find_by(id: session[:order_id]).all
     #@order = Order.last
+  end
+
+  private
+  def get_order
+    if session[:order_id]
+      @order = Order.find(session[:order_id])
+    else
+      @order = Order.create
+      session[:order_id] = @order.id
+    end
+  end
+
+  def get_referer
+    session[:return_to] ||= request.referer
   end
 end
