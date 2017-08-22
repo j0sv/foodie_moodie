00c544fd625eb093d5defce372da0b3272c34f53
faraznaeem <faraznaeem@gmail.com>
2017-08-22 09:56:45 +0200

    Add current location marker to map

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,6 +1,11 @@
+var my_latitude;
+var my_longitude;
+
 GMaps.geolocate({
   success: function(position) {
     map.setCenter(position.coords.latitude, position.coords.longitude);
+    my_latitude = position.coords.latitude;
+    my_longitude = position.coords.longitude;
   },
   error: function(error) {
     alert('Geolocation failed: '+error.message);
@@ -9,5 +14,13 @@ GMaps.geolocate({
     alert("Your browser does not support geolocation");
   },
   always: function() {
+      map.addMarker({
+          lat: my_latitude,
+          lng: my_longitude,
+          title: 'Lima',
+          click: function(e) {
+              alert('You clicked in this marker');
+          }
+      });
   }
 });
