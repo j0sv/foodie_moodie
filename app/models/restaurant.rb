4ca49b2664137c17bc8a421af30894e57ad26bdb
faraznaeem <faraznaeem@gmail.com>
2017-08-22 09:59:28 +0200

    Fix bug

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,7 +2,7 @@ require 'geocoder'
 
 class Restaurant < ApplicationRecord
   geocoded_by :address
-  after_validation :geocode, if: ->(obj){ obj.address.present? and obj.address_changed? }
+  after_validation :geocode
   has_many :dishes
   has_many :orders
 
