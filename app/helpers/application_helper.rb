a10b0eaa8917c87a3d09f3ddc791a4da37062124
j0sv <svensson.jonas@gmail.com>
2017-08-24 10:17:33 +0200

    Refactor varible and removed alert used for test

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,7 @@
 module ApplicationHelper
+  DEFAULT_IP_LOCATION = '195.67.156.197'
+  LOCALHOST_IP = '127.0.0.1'
+
   def get_order_items_count
     if session[:order_id]
       Order.find(session[:order_id]).total_unique_items
@@ -8,9 +11,8 @@ module ApplicationHelper
   end
 
   def remote_ip
-    if request.remote_ip == '127.0.0.1'
-      # Hard coded remote address
-      '195.67.156.197'
+    if request.remote_ip == LOCALHOST_IP
+      DEFAULT_IP_LOCATION
     else
       request.remote_ip
     end
