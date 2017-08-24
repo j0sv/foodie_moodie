8ef010940e59d396d0b9cd78daa3e7f7894f3c9e
j0sv <svensson.jonas@gmail.com>
2017-08-24 05:29:32 +0200

    Filter restaurants based on location
    Restaurants are filtered by location. Location can be
    entered by user in search field or by looking up
    the user IP address.
    Restaurants are displayed based on this location

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -3,9 +3,13 @@ class RestaurantsController < ApplicationController
   def index
 
     if params[:search].present?
-      @restaurants = Restaurant.near(params[:id])
+      @restaurants = Restaurant.near(params[:search])
+      @my_ip_location = Geocoder.coordinates(params[:search])
+      @geo_locate = false
     else
       @restaurants = Restaurant.near(helpers.remote_ip)
+      @my_ip_location = Geocoder.coordinates(helpers.remote_ip)
+      @geo_locate = true
     end
 
   end
