bff353bfb4982c7eb5795f3f04e6ec9b7a97c603
j0sv <svensson.jonas@gmail.com>
2017-08-29 08:17:25 +0200

    Modify ApplicationHelper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,2 @@
 class ApplicationController < ActionController::Base
-  before_action :get_order
-  helper ApplicationHelper
-  protect_from_forgery with: :exception
-
-private
-  def get_order
-    if session[:order_id]
-      @order = Order.find(session[:order_id])
-    else
-      @order = Order.create
-      session[:order_id] = @order.id
-    end
-  end
 end
