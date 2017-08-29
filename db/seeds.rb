047e8f9f6789d363cafa6076f9c16160fd969c73
j0sv <svensson.jonas@gmail.com>
2017-08-29 23:09:52 +0200

    Update seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,12 @@
+Restaurant.create(name: 'O Learys Kista', address1: 'Danmarksgatan 42', postal_code: '164 91', city: 'Kista', cuisine: 'Fast Food')
+Restaurant.create(name: 'Allé Grillen Rinkeby', address1: 'Rinkebysvängen', postal_code: '163 74', city: 'Spånga', cuisine: 'Fast Food')
+Restaurant.create(name: 'Flysta Pizzeria', address1: 'Spångavägen 321', postal_code: '163 46', city: 'Spånga', cuisine: 'Fast Food')
+Restaurant.create(name: 'Spånga Konditori', address1: 'Spånga Stationsplan 1', postal_code: '163 51', city: 'Spånga', cuisine: 'Fast Food')
+Restaurant.create(name: 'Viveka och Lottas', address1: 'Antenngatan 49B', postal_code: '421 33', city: 'Västra Frölunda', cuisine: 'Fast Food')
+Restaurant.create(name: 'Pizzeria Uzuner', address1: 'Rundradiogatan 4', postal_code: '421 35', city: 'Västra Frölunda', cuisine: 'Fast Food')
 
-Restaurant.create(name: 'Indian Fun', address1: 'Snorgatan 12', postal_code: '164 55', city: 'Kista', cuisine: 'Indian Food')
-Restaurant.create(name: 'Italian Fun', address1: 'Leyongatan 14', postal_code: '164 55', city: 'Kista', cuisine: 'Italian Food')
-Restaurant.create(name: 'Chinese Fun', address1: 'Potatisgatan 20', postal_code: '164 55', city: 'Kista', cuisine: 'Chinese Food')
-Restaurant.create(name: 'American Fun', address1: 'Snigelgatan 69', postal_code: '164 55', city: 'Kista', cuisine: 'American Food')
+DishCategory.create(name: 'Main')
+DishCategory.create(name: 'Starter')
+DishCategory.create(name: 'Dessert')
 
-Dish.create(title: 'papadom', description: 'deep fried crunchy things', price: 100, restaurant: Restaurant.first)
-
+Dish.create(title: 'Papadom', description: 'Deep fried crunchy things', price: 100, restaurant: Restaurant.first, dish_category: DishCategory.first)
